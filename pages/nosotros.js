73f21527ff2d7c7a6fac05f8fd30c2085d389fa8
visualiza <visualiza27@gmail.com>
2024-10-03 15:20:28 -0300

    Modifica el copy de Nosotros

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -4,15 +4,15 @@ import Navbar from 'components/Navbar'
 import cn from 'classnames'
 import Image from 'next/image'
 import banner from '../public/img/oficina.jpg'
-import {useAppWidth, useAppQuery} from 'context'
+import { useAppWidth, useAppQuery } from 'context'
 import Logo from 'components/svgs/Logo'
 // import logos from 'data/logos'
 import useMedia from 'hooks/useMedia'
 import useLoaded from 'hooks/useLoaded'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import ButtonWsp from 'components/ButtonWsp'
-import {getRandom} from 'utils'
-import {useQuery} from 'react-query'
+import { getRandom } from 'utils'
+import { useQuery } from 'react-query'
 import SEO from 'components/SEO'
 
 const metadata = {
@@ -30,7 +30,7 @@ export default function Home() {
 	const query = useAppQuery()
 	const isMobile = useMedia('(max-width: 767px)')
 	const loaded = useLoaded()
-	const {data: logos} = useQuery('logos')
+	const { data: logos } = useQuery('logos')
 	const [showLogos, setShowLogos] = React.useState(getRandom(logos, 12) ?? [])
 	const [showLogosMobile, setShowLogosMobile] = React.useState(
 		getRandom(logos, 1) ?? [],
@@ -69,7 +69,7 @@ export default function Home() {
 				gap: 2,
 			}
 		} else {
-			return {width: 6, height: 9, ml: 1}
+			return { width: 6, height: 9, ml: 1 }
 		}
 	}, [query])
 
@@ -99,9 +99,9 @@ export default function Home() {
 		<>
 			<SEO config={metadata} />
 			<motion.main
-				initial={{opacity: 0}}
-				animate={{opacity: loaded ? 1 : 0}}
-				transition={{duration: 0.8}}
+				initial={{ opacity: 0 }}
+				animate={{ opacity: loaded ? 1 : 0 }}
+				transition={{ duration: 0.8 }}
 				className={cn(
 					'flex flex-wrap mx-3',
 					isMobile ? 'mt-3' : 'my-3',
@@ -129,20 +129,13 @@ export default function Home() {
 										fontSize: 16,
 									}}
 								>
-									Somos un equipo multidisciplinario de arquitectos, diseñadores
-									y artistas. Todos enamorados del arte digital y la
-									visualización en tres dimensiones.
-									<br />
-									Disfrutamos que los proyectos sean un reto, que nos exijan
-									aprender constantemente. Queremos que nos busquen por un
-									resultado único, una metodología innovadora, y nuestro aporte
-									creativo en todo el proceso. Creemos en la oportunidad de
-									plasmar espacios aún no existentes y contar una historia en
-									ellos.
-									<br />
-									Compromiso, comunicación y pasión son partes fundamentales de
-									cada proyecto. Amamos lo que hacemos. <br />
-									Es así de simple.
+									En VISUALIZA transformamos tus proyectos inmobiliarios en experiencias visuales cautivadoras que impulsan las ventas y capturan la atención del mercado. Con más de una década de experiencia, somos especialistas en la creación de renders, recorridos virtuales y material audiovisual de alto impacto, diseñados para hacer que tus proyectos destaquen y se vendan con éxito.
+									<br></br>
+									Nuestro equipo multidisciplinario, compuesto por arquitectos, diseñadores y artistas apasionados por el arte digital, se enfrenta a cada nuevo desafío con entusiasmo y creatividad. Nos enorgullece adoptar una metodología innovadora que integra técnicas avanzadas de visualización en 3D, permitiéndonos plasmar espacios aún no construidos y narrar la historia única de cada proyecto.
+									<br></br>
+									En VISUALIZA, creemos que la clave de un proceso de venta efectivo radica en el compromiso, la comunicación abierta y la pasión por lo que hacemos. Nos esforzamos por ofrecer resultados excepcionales y personalizados, asegurando que cada cliente se sienta acompañado en cada etapa del proceso.
+									<br></br>
+									Deja que tu proyecto cobre vida con nosotros. Juntos, crearemos imágenes que no solo impresionan, sino que también convierten.
 								</p>
 							</section>
 							<section className="px-8 py-4 mb-10">
@@ -162,19 +155,19 @@ export default function Home() {
 							<section className="-ml-3 -mr-3 w-[calc(100%+1.5rem)] mt-9 relative">
 								<div
 									className="w-full h-full bg-black"
-									style={{minHeight: '240px'}}
+									style={{ minHeight: '240px' }}
 								>
 									<div className="w-full h-full">
-										{showLogosMobile?.map(({path, alt}) => (
+										{showLogosMobile?.map(({ path, alt }) => (
 											<motion.img
 												key={alt}
-												initial={{opacity: 0.6}}
-												animate={{opacity: 1}}
-												transition={{duration: 0.7}}
-												exit={{opacity: 0.6, transition: {duration: 0.5}}}
+												initial={{ opacity: 0.6 }}
+												animate={{ opacity: 1 }}
+												transition={{ duration: 0.7 }}
+												exit={{ opacity: 0.6, transition: { duration: 0.5 } }}
 												src={path}
 												alt={alt}
-												style={{maxHeight: 240}}
+												style={{ maxHeight: 240 }}
 												className="w-full !h-auto object-cover"
 											/>
 										))}
@@ -195,7 +188,7 @@ export default function Home() {
 							/>
 							<div
 								className={cn('flex flex-wrap')}
-								style={{marginBottom: isMobile && width}}
+								style={{ marginBottom: isMobile && width }}
 							>
 								{/* imgs */}
 								<div
@@ -218,22 +211,21 @@ export default function Home() {
 										<div
 											style={{
 												width: `${width * size.textWidth}px`,
-												height: `${
-													width *
+												height: `${width *
 													(query === 'xl' ? 12 : query === 'lg' ? 11 : 10)
-												}px`,
+													}px`,
 												marginLeft: width,
 												marginTop: `${width * size.titleMt}px`,
 												zIndex: 10,
 											}}
 										>
 											<h1
-												style={{height: width}}
+												style={{ height: width }}
 												className="flex flex-col justify-center font-sec text-5xl pl-3"
 											>
 												Somos
 											</h1>
-											<h2 style={{height: width}} className="pl-3">
+											<h2 style={{ height: width }} className="pl-3">
 												<Logo
 													color="black"
 													classname="w-[106px] md:w-[184px] lg:w-[186px] 2xl:w-[216px]"
@@ -250,17 +242,16 @@ export default function Home() {
 													style={{
 														background: '#fff',
 														overflow: isHovered ? 'auto' : 'hidden',
-														height: `${
-															width *
-																(query === 'xl' ? 7 : query === 'lg' ? 8 : 7) -
+														height: `${width *
+															(query === 'xl' ? 7 : query === 'lg' ? 8 : 7) -
 															1
-														}px`,
+															}px`,
 														lineHeight:
 															query === 'xl'
 																? 1.84
 																: query === 'lg'
-																? 1.7
-																: 1.7,
+																	? 1.7
+																	: 1.7,
 														fontSize:
 															query === 'xl' ? 17 : query === 'lg' ? 16 : 14,
 													}}
@@ -268,46 +259,37 @@ export default function Home() {
 													onMouseEnter={() => setIsHovered(true)}
 													onMouseLeave={() => setIsHovered(false)}
 												>
-													Somos un equipo multidisciplinario de arquitectos,
-													diseñadores y artistas. Todos enamorados del arte
-													digital y la visualización en tres dimensiones.
-													<br />
-													Disfrutamos que los proyectos sean un reto, que nos
-													exijan aprender constantemente. Queremos que nos
-													busquen por un resultado único, una metodología
-													innovadora, y nuestro aporte creativo en todo el
-													proceso. Creemos en la oportunidad de plasmar espacios
-													aún no existentes y contar una historia en ellos.
-													<br />
-													Compromiso, comunicación y pasión son partes
-													fundamentales de cada proyecto. Amamos lo que hacemos.{' '}
-													<br />
-													Es así de simple.
+													En VISUALIZA transformamos tus proyectos inmobiliarios en experiencias visuales cautivadoras que impulsan las ventas y capturan la atención del mercado. Con más de una década de experiencia, somos especialistas en la creación de renders, recorridos virtuales y material audiovisual de alto impacto, diseñados para hacer que tus proyectos destaquen y se vendan con éxito.
+													<br></br>
+													Nuestro equipo multidisciplinario, compuesto por arquitectos, diseñadores y artistas apasionados por el arte digital, se enfrenta a cada nuevo desafío con entusiasmo y creatividad. Nos enorgullece adoptar una metodología innovadora que integra técnicas avanzadas de visualización en 3D, permitiéndonos plasmar espacios aún no construidos y narrar la historia única de cada proyecto.
+													<br></br>
+													En VISUALIZA, creemos que la clave de un proceso de venta efectivo radica en el compromiso, la comunicación abierta y la pasión por lo que hacemos. Nos esforzamos por ofrecer resultados excepcionales y personalizados, asegurando que cada cliente se sienta acompañado en cada etapa del proceso.
+													<br></br>
+													Deja que tu proyecto cobre vida con nosotros. Juntos, crearemos imágenes que no solo impresionan, sino que también convierten.
 												</p>
 											</div>
 										</div>
 										<div
 											style={{
 												width: `${width * 24}px`,
-												height: `${
-													width *
+												height: `${width *
 													(query === 'xl' ? 10 : query === 'lg' ? 11 : 14)
-												}px`,
+													}px`,
 												marginLeft: `${width * size.ml}px`,
 												marginTop: query === 'xl' ? width : 0,
 											}}
 										>
 											<h3
 												className="font-sec text-[22px] md:text-[28px] pl-1 flex items-center"
-												style={{height: width}}
+												style={{ height: width }}
 											>
 												Nuestros clientes.
 											</h3>
 											<div
 												className="flex flex-wrap"
-												style={{marginTop: width, columnGap: width * size.gap}}
+												style={{ marginTop: width, columnGap: width * size.gap }}
 											>
-												{showLogos?.map(({path, alt}) => (
+												{showLogos?.map(({ path, alt }) => (
 													<motion.div
 														key={alt}
 														style={{
@@ -320,10 +302,10 @@ export default function Home() {
 													>
 														<motion.img
 															key={alt}
-															initial={{opacity: 0.6}}
-															animate={{opacity: 1}}
-															transition={{duration: 1.8}}
-															exit={{opacity: 0.6, transition: {duration: 0.9}}}
+															initial={{ opacity: 0.6 }}
+															animate={{ opacity: 1 }}
+															transition={{ duration: 1.8 }}
+															exit={{ opacity: 0.6, transition: { duration: 0.9 } }}
 															className="w-full !h-auto object-cover"
 															src={path}
 															alt={alt}

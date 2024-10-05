935a489821901bd2693c962002c855ce45bcee8d
visualiza <visualiza27@gmail.com>
2024-10-05 19:53:33 -0300

    Revert "Soluciona errores de compilacion"
    This reverts commit 552899c02e57c312b41115a0737344ef2985c2a3.

--- src/components/Card/CardPortfolio.js
+++ src/components/Card/CardPortfolio.js
@@ -27,8 +27,6 @@ function CardPortfolio({project, width, query}) {
 				<a role="button" onClick={e => open(e, id)}>
 					<Image
 						src={image}
-						width={1000}    
-						height={1000} 
 						className={cn(
 							'w-101 h-full absolute top-0 left-0 object-cover',
 							isHovered && '!filter !grayscale contrast-600 z-0',
@@ -38,6 +36,7 @@ function CardPortfolio({project, width, query}) {
 						onTouchStart={() => setIsHovered(true)}
 						onTouchEnd={() => setIsHovered(false)}
 						alt={project.alt}
+						placeholder="blur"
 					/>
 				</a>
 				<div

62535c918982c94d305a2104bb1996e69b6a088c
visualiza <visualiza27@gmail.com>
2024-10-05 17:24:17 -0300

    Agrega redirección a las imagenes de los nuevos proyectos

--- src/data/miniaturas.js
+++ src/data/miniaturas.js
@@ -1,8 +1,10 @@
-import image1 from '../../public/img/miniaturas/1.jpg'
+const image1 = 'https://res.cloudinary.com/dofrlsnep/image/upload/f_auto,q_auto/v1/miniaturas/n31tubeuybggbcquyjo2'
+
+  // import image1 from '../../public/img/miniaturas/1.jpg'
 import image2 from '../../public/img/miniaturas/2.jpg'
 import image3 from '../../public/img/miniaturas/3.jpg'
 import image4 from '../../public/img/miniaturas/4.jpg'
-import image5 from '../../public/img/miniaturas/5.jpg'
+const image5 = 'https://res.cloudinary.com/dofrlsnep/image/upload/f_auto,q_auto/v1/miniaturas/kcutvziqvpdfmq1mp3dg'
 import image6 from '../../public/img/miniaturas/6.jpg'
 import image7 from '../../public/img/miniaturas/7.jpg'
 import image8 from '../../public/img/miniaturas/8.jpg'
@@ -37,6 +39,38 @@ import image36 from '../../public/img/miniaturas/36.jpg'
 import image37 from '../../public/img/miniaturas/37.jpg'
 import image38 from '../../public/img/miniaturas/38.jpg'
 import image39 from '../../public/img/miniaturas/39.jpg'
+import image40 from '../../public/img/miniaturas/40.jpg'
+import image41 from '../../public/img/miniaturas/41.jpg'
+import image42 from '../../public/img/miniaturas/42.jpg'
+import image43 from '../../public/img/miniaturas/43.jpg'
+import image44 from '../../public/img/miniaturas/44.jpg'
+import image45 from '../../public/img/miniaturas/45.jpg'
+import image46 from '../../public/img/miniaturas/46.jpg'
+import image47 from '../../public/img/miniaturas/47.jpg'
+import image48 from '../../public/img/miniaturas/48.jpg'
+import image49 from '../../public/img/miniaturas/49.jpg'
+import image50 from '../../public/img/miniaturas/50.jpg'
+import image51 from '../../public/img/miniaturas/51.jpg'
+import image52 from '../../public/img/miniaturas/52.jpg'
+import image53 from '../../public/img/miniaturas/53.jpg'
+import image54 from '../../public/img/miniaturas/54.jpg'
+import image55 from '../../public/img/miniaturas/55.jpg'
+import image56 from '../../public/img/miniaturas/56.jpg'
+import image57 from '../../public/img/miniaturas/57.jpg'
+import image58 from '../../public/img/miniaturas/58.jpg'
+import image59 from '../../public/img/miniaturas/59.jpg'
+import image60 from '../../public/img/miniaturas/60.jpg'
+import image61 from '../../public/img/miniaturas/61.jpg'
+import image62 from '../../public/img/miniaturas/62.jpg'
+import image63 from '../../public/img/miniaturas/63.jpg'
+import image64 from '../../public/img/miniaturas/64.jpg'
+import image65 from '../../public/img/miniaturas/65.jpg'
+import image66 from '../../public/img/miniaturas/66.jpg'
+import image67 from '../../public/img/miniaturas/67.jpg'
+import image68 from '../../public/img/miniaturas/68.jpg'
+import image69 from '../../public/img/miniaturas/69.jpg'
+import image70 from '../../public/img/miniaturas/70.jpg'
+
 
 const miniaturas = {
 	image1,
@@ -78,5 +112,37 @@ const miniaturas = {
 	image37,
 	image38,
 	image39,
+	image40,
+	image41,
+	image42,
+	image43,
+	image44,
+	image45,
+	image46,
+	image47,
+	image48,
+	image49,
+	image50,
+	image51,
+	image52,
+	image53,
+	image54,
+	image55,
+	image56,
+	image57,
+	image58,
+	image59,
+	image60,
+	image61,
+	image62,
+	image63,
+	image64,
+	image65,
+	image66,
+	image67,
+	image68,
+	image69,
+	image70,
+
 }
 export default miniaturas

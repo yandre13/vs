a791d890168745bbf93ad468b7a65361044ed2af
Visualizape <visualiza27@gmail.com>
2025-04-25 15:58:14 -0300

    oculta un proyecto en pantallas grandes

--- src/components/List/ListPortfolio.js
+++ src/components/List/ListPortfolio.js
@@ -1,25 +1,31 @@
 import React from 'react'
 
-import {CardPortfolio} from 'components/Card/CardPortfolio'
-
-import {data} from 'data'
-import {useAppQuery, useAppWidth} from 'context'
+import { CardPortfolio } from 'components/Card/CardPortfolio'
+import { data } from 'data'
+import { useAppQuery, useAppWidth } from 'context'
 
 export default function ListPortfolio() {
-	const [width] = useAppWidth()
-	const query = useAppQuery()
-	return (
-		<>
-			{data.map(project => {
-				return (
-					<CardPortfolio
-						key={project.id}
-						project={project}
-						width={width}
-						query={query}
-					/>
-				)
-			})}
-		</>
-	)
+  const [width] = useAppWidth()
+  const query = useAppQuery()
+
+  return (
+    <>
+      {data.map(project => (
+        <div
+          key={project.id}
+          className={
+            (project.id === 54)
+              ? 'xl:hidden'   // oculta en pantallas ≥ 2xl
+              : ''
+          }
+        >
+          <CardPortfolio
+            project={project}
+            width={width}
+            query={query}
+          />
+        </div>
+      ))}
+    </>
+  )
 }

e5ed8f421a1d92dc3b84b4721284a6ed4d06e82a
yandre13 <yandre13.elsf@gmail.com>
2025-05-21 12:36:31 -0500

    feat: build

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 // next.config.js
 module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**', // Permite todas las rutas dentro de este dominio
-      },
-    ],
-  },
-};
+	output: 'export',
+	images: {
+		unoptimized: true,
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'res.cloudinary.com',
+				port: '',
+				pathname: '/**', // Permite todas las rutas dentro de este dominio
+			},
+		],
+	},
+}
